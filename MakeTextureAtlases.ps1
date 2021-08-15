38123a1f4d4f580997fb255acc8135354e0dbfaa
Steven Blom <atomicblom@binaryvibrance.net>
2021-08-15 12:14:21 +0930

    Starting to look at theming Myra

---
+++ MakeTextureAtlases.ps1
@@ -0,0 +1,5 @@
+﻿$texturePackerPath = "C:\Users\codew\Downloads\MyraTexturePacker.0.9.2\MyraTexturePacker.exe"
+
+Push-Location $PSScriptRoot
+& $texturePackerPath ".\Assets\UI" ".\FrontEnd\Slate.Client\Content\UI\UI.png"
+Pop-Location
